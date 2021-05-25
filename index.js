4832d8ccf8b049840ce73fc7b5f16b2356f58f42
Kaia <kbergmanndean@gmail.com>
2021-05-25 17:14:00 -0400

    commit tree 7dafcc0b4b74046800192845c3cb469014f6fda2

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Add your code here
+const submitData= function(userName, userEmail) {
+    return fetch("http://localhost:3000/users",{
+        method:"POST",
+        headers: {
+            "Content-Type":"application/json",
+            "Accept":"application/json",
+        },
+        body:JSON.stringify({
+            "name":userName,
+            "email":userEmail
+        })
+    }).then(response=> response.json())
+    .then(data=>renderId(data))
+    .catch((error)=> {
+        document.body.innerHTML=error.message
+    
+})
+}
+
+const renderId=data=> {
+    document.body.innerHTML=data.id
+}
